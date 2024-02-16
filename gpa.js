bddc8faa51cbac4fe932403ab8bcdafce6888c6f
Drasec Goner <drasec.04@gmail.com>
2024-02-16 14:51:34 +0530

    Practice FrontEnd Web

--- gpa.js
+++ gpa.js
@@ -1,3 +1,31 @@
+document.getElementById("gpaForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var course1 = document.getElementById("course1").value;
+    var grade1 = document.getElementById("grade1").value;
+    var course2 = document.getElementById("course2").value;
+    var grade2 = document.getElementById("grade2").value;
+    var course3 = document.getElementById("course3").value;
+    var grade3 = document.getElementById("grade3").value;
+    var course4 = document.getElementById("course4").value;
+    var grade4 = document.getElementById("grade4").value;
+    var course5 = document.getElementById("course5").value;
+    var grade5 = document.getElementById("grade5").value;
+    var course6 = document.getElementById("course6").value;
+    var grade6 = document.getElementById("grade6").value;
+    var course7 = document.getElementById("course7").value;
+    var grade7 = document.getElementById("grade7").value;
+    var course8 = document.getElementById("course8").value;
+    var grade8 = document.getElementById("grade8").value;
+    
+    var courses = [course1,course2,course3,course4,course5,course6,course7,course8];
+    var grades = [grade1,grade2,grade3,grade4,grade5,grade6,grade7,grade8];
+    // Calculate GPA
+    var cgpa = gpa(courses,grades);
+
+    // Display the GPA
+    document.getElementById("output").innerHTML = "Your GPA is: " + cgpa;
+});
 
 
 function gradeToMarks(grade) {
@@ -44,8 +72,7 @@ function gpa(subjects, grades) {
     var marks = [];
     var sumCreds =0;
     for (var i = 0; i < subjects.length; i++) {
-        var grade = grades[i].toUpperCase();
-        var mark = gradeToMarks(grade) * creds[subjects[i]];
+        var mark = gradeToMarks(grades[i]) * creds[subjects[i]];
         sumCreds+=creds[subjects[i]];
         marks.push(mark);
     }
